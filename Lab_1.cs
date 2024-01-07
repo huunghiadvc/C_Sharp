d0bcf66412abdb5f51e99940d4e677b3a87f9e65
huunghiadvc <mos98p@gmail.com>
2024-01-07 22:02:21 +0700

    upt

--- Lab_1.cs
+++ Lab_1.cs
@@ -8,16 +8,18 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            string str = "11-12-1998";
+            string str = "1997/1/06";
 
             string[] strArr = Utils.SplitString(str);
 
+            // Trường hợp sai format ngày, tháng, năm
             if (strArr.Length != 3)
             {
                 Console.WriteLine("Invalid date");
                 return;
             }
 
+            // validate ngày tháng năm
             if (!Utils.DateValid(strArr))
             {
                 return;
@@ -41,10 +43,12 @@ namespace Lab1
             }
             return strArr;
         }
+
         public static Boolean DateValid(string[] strArr)
         {
             int DateInt, MonthInt, YearInt;
 
+            // Convert string sang date, month, year theo kiểu int
             MonthInt = Convert.ToInt32(strArr[1]);
             if (strArr[0].Length == 2)
             {
@@ -57,22 +61,21 @@ namespace Lab1
                 YearInt = Convert.ToInt32(strArr[0]);
             }
 
+            // validate năm sinh
             if (YearInt > DateTime.Now.Year || YearInt < 0)
             {
                 Console.WriteLine("Invalid year: " + YearInt);
                 return false;
             }
 
-            int DayOfFeb;
-            if (DateTime.IsLeapYear(YearInt))
+            // lấy số ngày tháng 2
+            int DayOfFeb = 28;
+            if (MonthInt == 2 && DateTime.IsLeapYear(YearInt))
             {
                 DayOfFeb = 29;
             }
-            else
-            {
-                DayOfFeb = 28;
-            }
 
+            // validate tháng và số ngày trong tháng
             switch (MonthInt)
             {
                 case 1:
@@ -106,14 +109,15 @@ namespace Lab1
                     }
                     break;
             }
-
             return true;
         }
+
         public static int GetSumOfBirthDay(string dateOfBirth)
         {
             int dateOfBirthInt = Convert.ToInt32(dateOfBirth);
             return TotalDigit(dateOfBirthInt);
         }
+
         public static int TotalDigit(int number)
         {
             int result = 0, temp;
@@ -131,47 +135,50 @@ namespace Lab1
     {
         public static void ShowResult(int sum)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             string result = "";
+            int finalSum;
             if (sum == 22)
             {
-                Console.WriteLine("Tổng : " + sum);
-                Console.WriteLine("Tổng cuối : " + 4);
-                Console.WriteLine("Vô địch thiên hạ");
-                return;
+                finalSum = 4;
+                result = "Vô địch thiên hạ!";
             }
-            int finalSum = Utils.TotalDigit(sum);
-            switch (finalSum)
+            else
             {
-                case 2:
-                    result = "Kết quả 2";
-                    break;
-                case 3:
-                    result = "Kết quả 3";
-                    break;
-                case 4:
-                    result = "Kết quả 4";
-                    break;
-                case 5:
-                    result = "Kết quả 5";
-                    break;
-                case 6:
-                    result = "Kết quả 6";
-                    break;
-                case 7:
-                    result = "Kết quả 7";
-                    break;
-                case 8:
-                    result = "Kết quả 8";
-                    break;
-                case 9:
-                    result = "Kết quả 9";
-                    break;
-                case 10:
-                    result = "Kết quả 10";
-                    break;
-                case 11:
-                    result = "Kết quả 11";
-                    break;
+                finalSum = Utils.TotalDigit(sum);
+                switch (finalSum)
+                {
+                    case 2:
+                        result = "Kết quả 2";
+                        break;
+                    case 3:
+                        result = "Kết quả 3";
+                        break;
+                    case 4:
+                        result = "Kết quả 4";
+                        break;
+                    case 5:
+                        result = "Kết quả 5";
+                        break;
+                    case 6:
+                        result = "Kết quả 6";
+                        break;
+                    case 7:
+                        result = "Kết quả 7";
+                        break;
+                    case 8:
+                        result = "Kết quả 8";
+                        break;
+                    case 9:
+                        result = "Kết quả 9";
+                        break;
+                    case 10:
+                        result = "Kết quả 10";
+                        break;
+                    case 11:
+                        result = "Kết quả 11";
+                        break;
+                }
             }
             Console.WriteLine("Tổng : " + sum);
             Console.WriteLine("Tổng cuối : " + finalSum);
